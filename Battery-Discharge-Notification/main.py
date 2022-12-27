2b6e896679a5b94d05f0daa4f8369070b7a3d928
Arslan <66907380+antispambo@users.noreply.github.com>
2022-12-27 23:16:06 +0300

    app

---
+++ Battery-Discharge-Notification/main.py
@@ -0,0 +1,14 @@
+import psutil
+
+battery = psutil.sensors_battery()
+plugged = battery.power_plugged
+percent = battery.percent
+
+if percent <= 25 and plugged != True:
+    from pynotifier import Notification
+
+    Notification(
+        title = "Battery Low",
+        description = str(percent) + "% Battery remain!!",
+        duration = 5,
+    ).send()
